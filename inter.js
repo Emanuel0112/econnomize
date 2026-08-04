fc21b1b9005abefb66281d9e941ef2f74e37ce9c
Emanuel <emanueldospassos.12@gmail.com>
2026-08-03 22:26:09 -0300

    Primeira versão profissional

---
+++ inter.js
@@ -0,0 +1,19 @@
+// inter.js
+console.log("Bem-vindo à Rede Enconnomize!");
+
+// Função para abrir o WhatsApp (com o link wa.me/message oficial)
+function abrirWhatsApp() {
+  const url = "https://wa.me/message/2EZZLYQORZS3I1"; 
+  window.open(url, "_blank");
+}
+
+// Função para abrir o Instagram
+function abrirInstagram() {
+  const url = "https://www.instagram.com/encon.nomize?igsh=ZTB3cWhkamM5c2w2";
+  window.open(url, "_blank");
+}
+
+// Função de exemplo para o terceiro botão
+function mostrarAlerta() {
+  alert("Obrigado por entrar em contato com a Rede Enconnomize!");
+}
